d0d11416daaffb61a8e62ee394861a0bb0d652a8
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2025-04-11 14:47:53 -0700

    Added Timekeeper. Added obstacle to drop. Currently drops after 3 seconds but this can be adjusted. This can be set up via a trigger to fall and the player needs to avoid it.

---
+++ ObstacleDodge/Assets/Scripts/DroppingObject/DroppingObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDroppingObject
+{
+    void Drop();
+    void SetRendererEnabled(bool enabled);
+}
+
+public class DroppingObject : MonoBehaviour, IDroppingObject
+{
+    private Renderer theRenderer;
+    private Rigidbody theRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        theRenderer = GetComponent<Renderer>();
+        theRigidbody = GetComponent<Rigidbody>();
+        theRigidbody.useGravity = false;
+        SetRendererEnabled(false);
+
+        StartCoroutine(SetVisibleAndDrop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Drop()
+    {
+        theRigidbody.useGravity = true;
+    }
+
+    public void SetRendererEnabled(bool enabled)
+    {
+        Debug.Log("Setting renderer enabled to : " + enabled);
+        theRenderer.enabled = enabled;
+    }
+
+    IEnumerator SetVisibleAndDrop()
+    {
+        
+        yield return new WaitForSeconds(3);
+        Debug.Log("Start dropping object");
+        SetRendererEnabled(true);
+        Drop();
+    }
+}
---
+++ ObstacleDodge/Assets/Scripts/Timekeeper/Timekeeper.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Timekeeper : MonoBehaviour
+{
+    private Coroutine theRoutine;
+
+    private float timePassed = 0;
+
+    private int countSeconds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // theRoutine = StartCoroutine(CountOneSecond());
+        countSeconds = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed >= countSeconds + 1)
+        {
+            countSeconds++;
+            Debug.Log(countSeconds);
+        }
+    }
+
+    IEnumerator CountOneSecond()
+    {
+        while (true)
+        {
+            timePassed += 1;
+            Debug.Log("Time passed: " + timePassed);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
