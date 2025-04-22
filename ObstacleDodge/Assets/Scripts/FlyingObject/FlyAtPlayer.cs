1437240ca0da79401a7e7ec1f4967030eb9a0c62
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2025-04-22 15:22:24 -0700

    Added functionality to fly at player after 3 seconds, but only after parsing location, so that it doesn't follow the player.

--- ObstacleDodge/Assets/Scripts/FlyingObject/FlyAtPlayer.cs
+++ ObstacleDodge/Assets/Scripts/FlyingObject/FlyAtPlayer.cs
@@ -1,18 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class FlyAtPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private float speed = 1.0f;
+    private float timePassed = 0.0f;
+    private Vector3 parsedPlayerPosition;
+
+    private bool bStartMoving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(ParsePlayerLocation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(this.gameObject.transform.position, player.gameObject.transform.position, speed * Time.deltaTime);
+        if (bStartMoving)
+        {
+            transform.position = Vector3.MoveTowards(this.gameObject.transform.position, parsedPlayerPosition,speed * Time.deltaTime);
+        }
+        
+    }
+
+    IEnumerator ParsePlayerLocation()
+    {
+        yield return new WaitForSeconds(3.0f);
+        parsedPlayerPosition = player.transform.position;
+        bStartMoving = true;
     }
 }
